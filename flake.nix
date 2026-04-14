64df09ad22f8dabcc771c5a4360d51f788fe0061
Aiman Daniel <aiman2301@gmail.com>
2026-04-14 17:30:08 +0800

    nix: add act 0.2.86

--- flake.nix
+++ flake.nix
@@ -3,13 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/ab7b6889ae9d484eed2876868209e33eb262511d";
+    nixpkgs-act.url = "github:NixOS/nixpkgs/13043924aaa7375ce482ebe2494338e058282925";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-act, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        act-pkg = nixpkgs-act.legacyPackages.${system}.act;
       in
       {
         devShells.default = pkgs.mkShell {
@@ -19,11 +21,12 @@
             gotools
             go-tools
             sqlite
-          ];
+          ] ++ [ act-pkg ];
 
           shellHook = ''
             export GO111MODULE=on
-            echo "Go $(go version) is ready!"
+            echo "$(go version)"
+            echo "$(act --version)"
           '';
         };
       }
