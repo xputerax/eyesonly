9a28aade36ede3f7a28d002bf3cdca44093d4699
Aiman Daniel <aiman2301@gmail.com>
2026-04-15 11:25:04 +0800

    [skip ci] deps: nix: add apple-sdk_15 (fixes go 1.26 in nix)

--- flake.nix
+++ flake.nix
@@ -2,27 +2,27 @@
   description = "eyesonly - secure secret sharing";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/ab7b6889ae9d484eed2876868209e33eb262511d";
-    nixpkgs-go.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-act.url = "github:NixOS/nixpkgs/13043924aaa7375ce482ebe2494338e058282925";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-go, nixpkgs-act, flake-utils }:
+  outputs = { self, nixpkgs, nixpkgs-act, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        go-pkgs = nixpkgs-go.legacyPackages.${system};
         act-pkg = nixpkgs-act.legacyPackages.${system}.act;
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            go_1_26
+            apple-sdk_15
             gopls
             gotools
             go-tools
             sqlite
-          ] ++ [ go-pkgs.go_1_26 act-pkg ];
+          ] ++ [ act-pkg ];
 
           shellHook = ''
             export GO111MODULE=on
